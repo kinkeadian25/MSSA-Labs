649a4a0ec908905bda27399c107a29944b46b1f8
Ian Kinkead <96138825+kinkeadian25@users.noreply.github.com>
2022-10-04 12:02:50 -0700

    Add files via upload

---
+++ NumericVariables_CS/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericVariables_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Int
+            int courseID;
+            int lengthInWeeks; 
+            #endregion
+            double myGPA;
+            #region Strings
+            string courseTitle;
+            string courseDescription;
+            string instructorName;
+            #endregion
+            #region BooleanVar
+            bool doesExist;
+            bool passingGrade;
+            bool isEnrolled;
+            #endregion
+
+            #region DeclaredVariables
+            courseID = 101;
+            lengthInWeeks = 17;
+            myGPA = 3.4;
+            courseTitle = "PCAD8";
+            courseDescription = "Extensive training in Transact-SQL/Data and Access | Programming in C# | Programming in HTML5 with JavaScript & CSS3 | Azure Fundamentals | Azure Development | ASP.NET Core MVC application development.";
+            instructorName = "Brian Nielsen";
+            doesExist = true;
+            passingGrade = true;
+            isEnrolled = true; 
+            #endregion
+
+            if (doesExist)
+            {
+                Console.WriteLine(courseTitle);
+                Console.WriteLine(courseDescription);
+                Console.WriteLine(instructorName);
+                Console.WriteLine(doesExist);
+                Console.WriteLine(passingGrade);
+                Console.WriteLine(isEnrolled);
+                Console.WriteLine(courseID);
+                Console.WriteLine(lengthInWeeks);
+                Console.WriteLine(myGPA);
+                Console.ReadKey(); 
+            }
+        }
+    }
+}
